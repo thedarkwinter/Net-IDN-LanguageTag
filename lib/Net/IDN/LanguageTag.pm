b8f6ceb8bb1fe9789ca1a3fe08f7e420746d9d6b
Michael Holloway <michael@thedarkwinter.com>
2013-12-18 13:14:26 +0100

    clean-up / convert to dispatchers / added lots of Skipped tests

--- lib/Net/IDN/LanguageTag.pm
+++ lib/Net/IDN/LanguageTag.pm
@@ -4,23 +4,42 @@ package Net::IDN::LanguageTag;
 
 =head1 NAME
 
-Net::IDN::LanguageTag - Tool to convert between different LanguageTag systems poorly based in RFC 5646
+Net::IDN::LanguageTag - Tool to convert between different LanguageTag systems poorly based in RFC5646
 
 =head1 SYNOPSIS
 
-	my $lt;
-  $lt = Net::IDN::LanguageTag->new(iso639_1  => 'en' ); # define input
-  $lt = Net::IDN::LanguageTag->new('en' ); # auto_detect
-  $lt->_from('iso639_1','en'); # call internal method
+  my $lt;
+  $lt = Net::IDN::LanguageTag->new('en'); # auto_detect
+  $lt->_from_iso639_1('en'); # call internal method
   $lt->iso639_2() # get value
 
 =head1 DESCRIPTION
 
 The author was too lazy to write a description.
 
-=head1 METHODS
+=head1 ACCESSORS
 
-Accessors: language iso639_1 iso639_2 iso15924 iso15924_numeric
+=head3 iso639_1()
+
+=head3 iso639_2()
+
+=head3 iso3166_1()
+
+=head3 iso15924()
+
+=head3 iso15924_numeric()
+
+=head3 language()
+
+=head3 country()
+
+=head3 script()
+
+=head3 variant()
+
+=head3 is_grandfather()
+
+=head3 is_private()
 
 =cut
 
@@ -34,13 +53,36 @@ use Data::Dumper;
 
 
 use base qw/Class::Accessor/;
-our @FORMATS=qw(country language script variant is_grandfather is_private iso3166_1 iso639_1 iso639_2 iso15924 iso15924_numeric);
-__PACKAGE__->mk_accessors(@FORMATS);
+our @ATTRS=qw(is_grandfather is_private);
+our @FORMATS=qw(country language script variant iso3166_1 iso639_1 iso639_2 iso15924 iso15924_numeric);
+__PACKAGE__->mk_accessors((@ATTRS,@FORMATS));
 
 our $VERSION = '0.04';
 
 ################################################################################
-### MAIN subs
+=pod
+
+=head1 PUBLIC METHODS
+
+=head2 new()
+
+Create a new LanguageTag object and optionionally pass a language tag string to automically parse it.
+
+  my $lt = Net::IDN::LanguageTag->new('eng-Cyrl');
+
+=head2 parse()
+
+Parse a RFC5646 string
+
+  $lt->parse('en-GB');
+  
+=head2 reset()
+
+Reset (undef) all accessors. You probably won't need to call it as its done automically on parse()
+
+  $lt->reset(); 
+  
+=cut
 
 sub new {
 	my ($class,$lt) = @_;
@@ -49,44 +91,67 @@ sub new {
 	return $self;
 }
 
+sub parse {
+	my ($self,$lt) = @_;
+	$self->reset();
+	my %h = $self->_detect($lt);
+	
+#	print Dumper \%h;
+	
+	foreach my $f (keys %h)
+	{
+    $self->_from($f,$h{$f});
+    $self->variant($h{$f}) if $f eq 'variant';
+  }
+	return;
+}
+
 # reset all values
-sub _reset {
+sub reset {
 	my $self = shift;
-	our (@FORMATS);
-	foreach (@FORMATS) {
+	our (@ATTRS,@FORMATS);
+	foreach ((@ATTRS,@FORMATS)) {
 		$self->$_(undef);
 	}
 	return;
 }
 
-sub parse {
-	my ($self,$lt) = @_;
-	$self->_reset();
-	my %h = $self->detect($lt);
-#	print Dumper \%h;
-	
-	foreach my $k (keys %h)
-	{
-    $self->_from_iso639_1($h{$k}) if $k eq 'iso639_1';
-    $self->_from_iso639_2($h{$k}) if $k eq 'iso639_2';
-    $self->_from_iso15924($h{$k}) if $k eq 'iso15924';
-    $self->_from_iso3166_1($h{$k}) if $k eq 'iso3166_1';
-    $self->_from_language($h{$k}) if $k eq 'language';
-    $self->_from_country($h{$k}) if $k eq 'country';
-    $self->_from_script($h{$k}) if $k eq 'script';
-    $self->variant($h{$k}) if $k eq 'variant';
+################################################################################
+=pod
+
+=head1 PRIVATE METHODS
+
+In most cases, you should not need to call these, but be my guest.
+
+=head2 _detect()
+
+Detects what format the string is in, and returns a hash of format => value after splitting it.
+
+  %h = $lt->_detect('eng-Latn'); # ( 'iso639_1' => 'en', 'iso15924' => 'Latn')
+
+=cut
+
+sub _detect {
+  my ($self,$v) = @_;
+  our @FORMATS;
+  my %h;
+  foreach my $s (split '-',$v)
+  {
+      $self->is_grandfather(1) && next if ($s eq 'i');
+      $self->is_private(1) && next if ($s eq 'x');
+      foreach my $f (@FORMATS)
+      {
+        next if exists ($h{$f});
+        next if ($f =~ m/^iso639/ && ( exists $h{'iso639_1'} || exists $h{'iso639_2'})); # we assume only one will be used
+        $h{$f} = $s if ($self->_is($f,$s));
+      }
+      ## variants
   }
-	return;
+  return %h;
 }
 
-sub parse_hash {
-  my ($self,%h) = @_;
-
-   return;
-}
-
-
-sub detect {
+=cut
+sub _detect_old {
   my ($self,$v) = @_;
 
   # Singletons I & X
@@ -105,12 +170,12 @@ sub detect {
 
   if ($v3) # three parts
   {
-    return ( 'iso639_1' => $v1, 'iso3166-1' => $v2, 'variant' => $v3) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2) && $v3 =~ /^\w+$/; # en-GB-cockney ??
-    return ( 'iso639_2' => $v2, 'iso3166-1' => $v2, 'variant' => $v3) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2) && $v3 =~ /^\w+$/; # eng-GB-cockney ??
+    return ( 'iso639_1' => $v1, 'iso3166_1' => $v2, 'variant' => $v3) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2) && $v3 =~ /^\w+$/; # en-GB-cockney ??
+    return ( 'iso639_2' => $v2, 'iso3166_1' => $v2, 'variant' => $v3) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2) && $v3 =~ /^\w+$/; # eng-GB-cockney ??
   } elsif ($v2) # two parts
   {
-    return ( 'iso639_1' => $v1, 'iso3166-1' => $v2) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2); # en-GB
-    return ( 'iso639_2' => $v1, 'iso3166-1' => $v2) if $self->_is_iso639_2($v1) && $self->_is_iso3166_1($v2); # eng-GB
+    return ( 'iso639_1' => $v1, 'iso3166_1' => $v2) if $self->_is_iso639_1($v1) && $self->_is_iso3166_1($v2); # en-GB
+    return ( 'iso639_2' => $v1, 'iso3166_1' => $v2) if $self->_is_iso639_2($v1) && $self->_is_iso3166_1($v2); # eng-GB
     return ( 'iso639_1' => $v1, 'iso15924' => $v2) if $self->_is_iso639_1($v1) && $self->_is_iso15924($v2); # en-Latn
     return ( 'iso639_2' => $v1, 'iso15924' => $v2) if $self->_is_iso639_2($v1) && $self->_is_iso15924($v2); # eng-Latn
   } else # 1 part
@@ -125,25 +190,85 @@ sub detect {
   }
   return;
 }
+=cut
+
 
 ################################################################################
-### IS subs
-sub _is_iso639_1 { return ( $_[1] =~ m/^\w{2}$/ && code2language($_[1],LOCALE_LANG_ALPHA_2) ) ? 1 : undef; }
-sub _is_iso639_2 { return ( $_[1] =~ m/^\w{3}$/ && code2language($_[1],LOCALE_LANG_ALPHA_3) ) ? 1 : undef; }
-sub _is_iso3166_1 { return ( $_[1] =~ m/^\w{2}$/ && code2country($_[1],LOCALE_CODE_ALPHA_2) ) ? 1 : undef; }
-sub _is_iso15924 { return ( ($_[1] =~ m/^\w{4}$/ && code2script($_[1],LOCALE_SCRIPT_ALPHA)) || ($_[1] =~ m/^\d{3}$/ && code2script($_[1],LOCALE_SCRIPT_NUMERIC)) ) ? 1 : undef; }
+=pod
 
-sub _is_language { return ( $_[1] =~ m/^\w{3,}$/ && language2code($_[1]) ) ? 1 : undef; }
-sub _is_country { return ( $_[1] =~ m/^\w{3,}$/ && country2code($_[1]) ) ? 1 : undef; }
-sub _is_script {  return ( $_[1] =~ m/^\w{3,}$/ && script2code($_[1]) ) ? 1 : undef; }
+=head2 _is()
+
+Dispatcher to check if a string is <format>
+
+  print "yes it is" if $lr->_is('iso639_1','en');
+
+=cut
+
+our $is_dispatch = { 
+        'iso639_1' => sub { return ( $_[0] =~ m/^\w{2}$/ && code2language($_[0],LOCALE_LANG_ALPHA_2) ) ? 1 : undef; },
+        'iso639_2' => sub { return ( $_[0] =~ m/^\w{3}$/ && code2language($_[0],LOCALE_LANG_ALPHA_3) ) ? 1 : undef; },
+        'iso3166_1' => sub { return ( $_[0] =~ m/^\w{2}$/ && code2country($_[0],LOCALE_CODE_ALPHA_2) ) ? 1 : undef; },
+        'iso15924' => sub { return ( ($_[0] =~ m/^\w{4}$/ && code2script($_[0],LOCALE_SCRIPT_ALPHA)) || ($_[0] =~ m/^\d{3}$/ && code2script($_[0],LOCALE_SCRIPT_NUMERIC)) ) ? 1 : undef; },
+
+        'language' => sub { return ( $_[0] =~ m/^\w{3,}$/ && language2code($_[0]) ) ? 1 : undef; },
+        'country' => sub { return ( $_[0] =~ m/^\w{3,}$/ && country2code($_[0]) ) ? 1 : undef; },
+        'script' => sub {  return ( $_[0] =~ m/^\w{3,}$/ && script2code($_[0]) ) ? 1 : undef; },
+    };
+
+sub _is {
+  my ($self,$format,$what) = @_;
+  our ($is_dispatch);
+  return (exists $is_dispatch->{$format}) ? $is_dispatch->{$format}->($what) : undef;
+}
 
 ################################################################################
-### FROM subs
+=pod
+
+=head2  _from()
+
+Dispatcher to convert to everythong possible from <format>
+
+  $lt->from('iso639_1','en');
+  
+You can also call the methods direct
+
+=head3 _from_iso639_1()
+
+=head3 _from_iso639_2()
+
+=head3 _from_iso3166_1()
+
+=head3 _from_iso15924()
+
+=head3 _from_language()
+
+=head3 _from_country()
+
+=head3 _from_script()
+
+=cut
+
+our $from_dispatch = {
+      'iso639_1' => \&_from_iso639_1,
+      'iso639_2' => \&_from_iso639_2,
+      'iso3166_1' => \&_from_iso3166_1,
+      'iso15924' => \&_from_iso15924,
+      'language' => \&_from_language,
+      'country' => \&_from_country,
+      'script' => \&_from_script,
+  };
+
+sub _from {
+  my ($self,$format,$what) = @_;
+  our ($from_dispatch);
+  return (exists $from_dispatch->{$format}) ? $from_dispatch->{$format}->($self,$what) : undef;
+}
+
 sub _from_iso639_1
 {
   my ($self,$c) = @_;
-  return unless $self->_is_iso639_1($c);
-  $self->iso639_1($c);
+  return unless $self->_is('iso639_1',$c);
+  $self->iso639_1(lc($c));
   $self->iso639_2(language_code2code($c,LOCALE_LANG_ALPHA_2,LOCALE_LANG_ALPHA_3));
   $self->language(code2language($c,LOCALE_LANG_ALPHA_2));
   return;
@@ -152,8 +277,8 @@ sub _from_iso639_1
 sub _from_iso639_2
 {
   my ($self,$c) = @_;
-  return unless $self->_is_iso639_2($c);
-  $self->iso639_2($c);
+  return unless $self->_is('iso639_2',$c);
+  $self->iso639_2(lc($c));
   $self->iso639_1(language_code2code($c,LOCALE_LANG_ALPHA_3,LOCALE_LANG_ALPHA_2));
   $self->language(code2language($c,LOCALE_LANG_ALPHA_3));
   return;
@@ -162,8 +287,8 @@ sub _from_iso639_2
 sub _from_iso3166_1
 {
   my ($self,$c) = @_;
-  return unless $self->_is_iso3166_1($c);
-  $self->iso3166_1($c);
+  return unless $self->_is('iso3166_1',$c);
+  $self->iso3166_1(uc($c));
   $self->country(code2country($c,LOCALE_CODE_ALPHA_2));
   return;
 }
@@ -171,17 +296,17 @@ sub _from_iso3166_1
 sub _from_iso15924
 {
   my ($self,$c) = @_;
-  return unless $self->_is_iso15924($c);
+  return unless $self->_is('iso15924',$c);
   if ($c =~ m/^\w{4}$/)
   {
     $self->iso15924($c);
     $self->iso15924_numeric(script_code2code($c,LOCALE_SCRIPT_ALPHA,LOCALE_SCRIPT_NUMERIC));
-     $self->script(code2script($self->iso15924));
+     $self->script(code2script($self->iso15924()));
   } elsif ($c =~ m/^\d{3}$/)
   {
      $self->iso15924_numeric($c);
      $self->iso15924(script_code2code($c,LOCALE_SCRIPT_NUMERIC,LOCALE_SCRIPT_ALPHA));
-     $self->script(code2script($self->iso15924));
+     $self->script(code2script($self->iso15924()));
   }
   return;
 }
@@ -189,7 +314,7 @@ sub _from_iso15924
 sub _from_language
 {
   my ($self,$c) = @_;
-  return unless $self->_is_language($c);
+  return unless $self->_is('language',$c);
   $self->iso639_1(language2code($c));
   $self->iso639_2(language_code2code($self->iso639_1(),LOCALE_LANG_ALPHA_2,LOCALE_LANG_ALPHA_3));
   $self->language($c);
@@ -199,7 +324,7 @@ sub _from_language
 sub _from_country
 {
   my ($self,$c) = @_;
-  return unless $self->_is_country($c);
+  return unless $self->_is('country',$c);
   $self->iso3166_1(country2code($c));
   $self->country($c);
   return;
@@ -208,7 +333,7 @@ sub _from_country
 sub _from_script
 {
   my ($self,$c) = @_;
-  return unless $self->_is_script($c);
+  return unless $self->_is('script',$c);
   $self->iso15924(script2code($c));
   $self->iso15924_numeric(script_code2code($self->iso15924(),LOCALE_SCRIPT_ALPHA,LOCALE_SCRIPT_NUMERIC));
   $self->script($c);
@@ -219,12 +344,12 @@ sub _from_script
 
 =pod
 
-=head1 SUPPORT
-
-No support is available
-
 =head1 AUTHOR
 
-Copyright 2012 Anonymous.
+Copyright 2013 Michael Holloway <michael@thedarkwinter.com>
+
+=head1 LICENSE
+
+The Artistic License 2.0
 
 =cut
--- t/02_language_tag.t
+++ t/02_language_tag.t
@@ -3,85 +3,143 @@
 use 5.010;
 use strict;
 use warnings;
-use Test::More tests => 50;
+use Test::More tests => 83;
 use Net::IDN::LanguageTag;
 use Data::Dumper;
 
 my $lt = Net::IDN::LanguageTag->new();
 isa_ok($lt,'Net::IDN::LanguageTag');
 
-
+################################################################################
 ## Test the _IS_ISO methods
-is($lt->_is_iso639_1('en'),1,'_is_iso639_1 valid');
-is($lt->_is_iso639_1('za'),1,'_is_iso639_1 valid');
-is($lt->_is_iso639_1('qq'),undef,'_is_iso639_1 invalid');
-is($lt->_is_iso639_2('eng'),1,'_is_iso639_2 valid');
-is($lt->_is_iso639_2('fre'),1,'_is_iso639_2 valid');
-is($lt->_is_iso639_2('qqq'),undef,'_is_iso639_2 invalid');
-is($lt->_is_iso3166_1('GB'),1,'_is_iso3166_1 valid');
-is($lt->_is_iso3166_1('FR'),1,'_is_iso3166_1 valid');
-is($lt->_is_iso3166_1('QQ'),undef,'_is_iso3166_1 invalid');
-is($lt->_is_iso15924('Phnx'),1,'_is_iso15924 valid');
-is($lt->_is_iso15924(220),1,'_is_iso15924 valid');
-is($lt->_is_iso15924('Zaza'),undef,'_is_iso15924 invalid');
+is($lt->_is('iso639_1','en'),1,'_is_iso639_1 valid');
+is($lt->_is('iso639_1','za'),1,'_is_iso639_1 valid');
+is($lt->_is('iso639_1','qq'),undef,'_is_iso639_1 invalid');
+is($lt->_is('iso639_2','eng'),1,'_is_iso639_2 valid');
+is($lt->_is('iso639_2','fre'),1,'_is_iso639_2 valid');
+is($lt->_is('iso639_2','qqq'),undef,'_is_iso639_2 invalid');
+is($lt->_is('iso3166_1','GB'),1,'_is_iso3166_1 valid');
+is($lt->_is('iso3166_1','FR'),1,'_is_iso3166_1 valid');
+is($lt->_is('iso3166_1','QQ'),undef,'_is_iso3166_1 invalid');
+is($lt->_is('iso15924','Phnx'),1,'_is_iso15924 valid');
+is($lt->_is('iso15924',220),1,'_is_iso15924 valid');
+is($lt->_is('iso15924','Zaza'),undef,'_is_iso15924 invalid');
 
 ## Test the IS_{natural} methods
-is($lt->_is_language('English'),1,'_is_language valid');
-is($lt->_is_language('Entish'),undef,'_is_language invalid');
-is($lt->_is_country('France'),1,'_is_france valid');
-is($lt->_is_country('Neverland'),undef,'_is_france valid');
-is($lt->_is_script('Cyrillic'),1,'_is_script valid');
-is($lt->_is_script('Squiggles'),undef,'_is_script valid');
+is($lt->_is('language','English'),1,'_is_language valid');
+is($lt->_is('language','Entish'),undef,'_is_language invalid');
+is($lt->_is('country','France'),1,'_is_france valid');
+is($lt->_is('country','Neverland'),undef,'_is_france valid');
+is($lt->_is('script','Cyrillic'),1,'_is_script valid');
+is($lt->_is('script','Squiggles'),undef,'_is_script valid');
 
-
-## Test from _FROM methods
+################################################################################
+### Test from _FROM methods
 
 # FROM iso639_1
-$lt->_reset();
+$lt->reset();
 $lt->_from_iso639_1('en');
 is($lt->iso639_1(),'en','from iso639_1 -> iso639_1 (same)');
 is($lt->iso639_2(),'eng','from iso639_1 -> iso639_2');
 is($lt->language(),'English','from iso639_1 -> language');
 
 # FROM iso639_2
-$lt->_reset();
+$lt->reset();
 $lt->_from_iso639_2('eng');
 is($lt->iso639_1(),'en','from iso639_2 -> iso639_1');
 is($lt->iso639_2(),'eng','from iso639_2 -> iso639_2 (same)');
 is($lt->language(),'English','from iso639_2 -> language');
 
 # FROM iso3166_1
-$lt->_reset();
+$lt->reset();
 $lt->_from_iso3166_1('GB');
 is($lt->iso3166_1(),'GB','from iso3166_1 -> iso3166_1 (same)');
 is($lt->country(),'United Kingdom','from iso3166_1 -> country');
 
 # FROM iso15924
-$lt->_reset();
+$lt->reset();
 $lt->_from_iso15924('Phnx');
 is($lt->iso15924(),'Phnx','from iso15924 -> iso15924 (same)');
 
 # FROM language
-$lt->_reset();
+$lt->reset();
 $lt->_from_language('English');
 is($lt->iso639_1(),'en','from language -> iso639_1');
 is($lt->iso639_2(),'eng','from language -> iso639_2');
 is($lt->language(),'English','from language -> language (same)');
 
 # FROM country
-$lt->_reset();
+$lt->reset();
 $lt->_from_country('France');
 is($lt->iso3166_1(),'fr','from country -> iso3166_1');
 is($lt->country(),'France','from country  -> country (same)');
 
 # FROM script
-$lt->_reset();
+$lt->reset();
 $lt->_from_script('Cyrillic');
 is($lt->iso15924(),'Cyrl','from script -> iso15924');
 is($lt->iso15924_numeric(),'220','from script -> iso15924_numeric');
 is($lt->script(),'Cyrillic','from script -> script (same)');
 
+################################################################################
+# Test _detect 
+is_deeply( {$lt->_detect('en-GB')},{'iso639_1'=>'en','iso3166_1'=>'GB'},'_detect en-GB');
+is_deeply( {$lt->_detect('eng-ZA')},{'iso639_2'=>'eng','iso3166_1'=>'ZA'},'_detect eng-ZA');
 
+# RFC 'irregular'
+SKIP: {
+  skip "Irregular tags not [yet] supported",17;
+  is_deeply( {$lt->_detect('en-GB-oed')},{},'_detect irregular en-GB-oed');
+  is_deeply( {$lt->_detect('i-ami')},{},'_detect irregular i-ami');
+  is_deeply( {$lt->_detect('i-bnn')},{},'_detect irregular i-bnn');
+  is_deeply( {$lt->_detect('i-default')},{},'_detect irregular i-default');
+  is_deeply( {$lt->_detect('i-enochian')},{},'_detect irregular i-enochian');
+  is_deeply( {$lt->_detect('i-hak')},{},'_detect irregular i-hak');
+  is_deeply( {$lt->_detect('i-klingon')},{},'_detect irregular i-klingon');
+  is_deeply( {$lt->_detect('i-lux')},{},'_detect irregular i-lux');
+  is_deeply( {$lt->_detect('i-mingo')},{},'_detect irregular i-mingo');
+  is_deeply( {$lt->_detect('i-navajo')},{},'_detect irregular i-navajo');
+  is_deeply( {$lt->_detect('i-pwn')},{},'_detect irregular i-pwn');
+  is_deeply( {$lt->_detect('i-tao')},{},'_detect irregular i-tao');
+  is_deeply( {$lt->_detect('i-tay')},{},'_detect irregular i-tay');
+  is_deeply( {$lt->_detect('i-tsu')},{},'_detect irregular i-tsu');
+  is_deeply( {$lt->_detect('sgn-BE-FR')},{},'_detect irregular sgn-BE-FR');
+  is_deeply( {$lt->_detect('sgn-BE-NL')},{},'_detect irregular sgn-BE-NL');
+  is_deeply( {$lt->_detect('sgn-CH-DE')},{},'_detect irregular sgn-CH-DE');
+};
+
+#RFC 'grandfather' : is this still irregular ?
+SKIP: {
+  skip "Grandfathered tags not [yet] supported",1;
+  is_deeply( {$lt->_detect('i-ami')},{},'_detect grandfather i-ami');
+};
+
+#RFC 'private'
+SKIP: {
+  skip "Private tags not [yet] supported",3;
+  is_deeply( {$lt->_detect('en-x-US')},{},'_detect private en-x-US');
+  is_deeply( {$lt->_detect('el-x-koine')},{},'_detect private en-x-koine');
+  is_deeply( {$lt->_detect('el-x-attic')},{},'_detect private en-x-attic');
+};
+
+# RFC 'regular'
+is_deeply( {$lt->_detect('en-Latn')},{'iso639_1'=>'en','iso15924'=>'Latn'},'_detect en-Latn');
+SKIP: {
+  skip "Regular tags that are failing in this push",9;
+  is_deeply( {$lt->_detect('art-lojban')},{'iso639_2'=>'art','variant'=>'lojban'},'_detect art-lojban'); # TODO : check
+  is_deeply( {$lt->_detect('cel-gaulish')},{'iso639_2'=>'cel','variant'=>'gaulish'},'_detect cel-gaulish');
+  is_deeply( {$lt->_detect('no-bok')},{'iso639_1'=>'no','variant'=>'bok'},'_detect no-bok'); # TODO : check
+  is_deeply( {$lt->_detect('no-nyn')},{'iso639_1' =>'no','variant'=>'nyn'},'_detect no-nyn'); # TODO : check
+  is_deeply( {$lt->_detect('zh-guoyu')},{'iso639_1' =>'zh','variant'=>''},'_detect zh-guoyu'); # TODO : check
+  is_deeply( {$lt->_detect('zh-hakka')},{'iso639_1' =>'zh','variant'=>''},'_detect zh-hakka'); # TODO : check
+  is_deeply( {$lt->_detect('zh-min')},{'iso639_1' =>'zh','variant'=>''},'_detect zh-min'); # TODO : check
+  is_deeply( {$lt->_detect('zh-min-nan')},{'iso639_1' =>'zh','variant'=>''},'_detect zh-min-nan'); # TODO : check
+  is_deeply( {$lt->_detect('zh-xiang')},{'iso639_1' =>'zh','variant'=>''},'_detect zh-xiang'); # TODO : check
+};
+
+################################################################################
+
+# Test Autodetect
 # Autodetect [iso639_1]
 $lt->parse('zh');
 is ($lt->iso639_2(),'chi','AutoDetect [iso639_1]');
@@ -109,6 +167,8 @@ is ($lt->iso639_2(),'eng','AutoDetect iso639_2 [iso639_2-iso15924]');
 # Autodetect [language]
 $lt->parse('Chinese');
 is ($lt->iso639_2(),'chi','AutoDetect [language]');
+
+################################################################################
 
 # INVALID codes/language
 $lt->parse('ac');
